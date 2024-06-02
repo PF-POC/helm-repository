28363799dc2311826429c5e112e7f1550a172e77
paul foster <pafoster@redhat.com>
2024-06-02 14:02:08 +1000

    updated inital code

---
+++ charts/vault/scripts/configure-vault.sh
@@ -0,0 +1,37 @@
+# oc -n vault exec -it vault-0 -- sh 
+
+#!/bin/sh
+
+# Enable the kubernetes auth method
+vault auth enable kubernetes
+
+
+# Write kubernetes auth configuration
+vault write auth/kubernetes/config \
+    token_reviewer_jwt="$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" \
+    kubernetes_host="https://$KUBERNETES_PORT_443_TCP_ADDR:443" \
+    kubernetes_ca_cert=@/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
+
+# vault write auth/kubernetes/config \
+#     token_reviewer_jwt="<token> \
+#     kubernetes_host=https://api.rosa-pub-1.ttrc.p3.openshiftapps.com:443 \
+#     kubernetes_ca_cert=@/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
+
+# Enable kv secrets engine
+vault secrets enable -version=2 kv
+
+# Create our sample kv
+vault kv put kv/vplugin/supersecret username="myuser" password="password"
+
+# Create policy for secret access
+vault policy write vplugin - <<EOF
+path "kv/*" {
+  capabilities = ["read"]
+}
+EOF
+
+vault write auth/kubernetes/role/argocd \
+    bound_service_account_names=vplugin \
+    bound_service_account_namespaces=openshift-gitops \
+    policies=vplugin \
+    ttl=1h
